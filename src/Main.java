a48a21f64edc25d6ccbea191c9600c1314322326
qux <qux@garena.com>
2015-10-26 20:11:03 +0800

    cam dp solution not accepted

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,55 @@ class Main {
         }
     }
 
+
     public static void execute() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        int N;
+        int M;
+        while ( (line = readString(reader)) != null) {
+            String[] nums = line.trim().split(" ");
+            N = Integer.valueOf(nums[0]);
+            M = Integer.valueOf(nums[1]);
+            int[] cam = new int[N+1];
+            for (int i=0; i<N+1; ++i) {
+                cam[i] = readInt(reader);
+            }
+
+            int[][] dp = new int[N+1][M+1];
+            for (int i=0; i<M+1; i++) {
+                dp[0][i] = cam[0];
+            }
+
+            for (int i=0; i<N+1; i++) {
+                dp[i][0] = getSum(cam, 0, i);
+            }
+
+            for (int n=1; n<N+1; ++n) {
+                for (int m=1; m<M+1; ++m) {
+                    int min = 100000;
+                    for (int i=1; i<=n; ++i) {
+                        int sum = getSum(cam, n-i+1, n);
+                        int tmp = Math.max(dp[n-i][m-1], sum);
+                        if (tmp < min) {
+                            min = tmp;
+                        }
+                    }
+                    dp[n][m] = min;
+                }
+            }
+
+//            for (int n=0; n<N+1; n++) {
+//                for (int m=0; m<M+1; m++) {
+//                    System.out.println("dp[" + n + "][" + m + "]=" + dp[n][m]);
+//                }
+//            }
+
+            System.out.println(dp[N][M]);
+        }
+    }
+
+    public static void execute2() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         String line;
@@ -31,28 +79,69 @@ class Main {
         int M;
         while ( (line = readString(reader)) != null) {
             String[] nums = line.trim().split(" ");
-            N = Integer.valueOf(nums[0]) + 1;
-            M = Integer.valueOf(nums[1]) + 1;
-            int[] cam = new int[N];
-            for (int i=0; i<N; ++i) {
+            N = Integer.valueOf(nums[0]);
+            M = Integer.valueOf(nums[1]);
+            int[] cam = new int[N+1];
+            for (int i=0; i<N+1; ++i) {
                 cam[i] = readInt(reader);
             }
 
-            int biggest = cam[0];
-            for (int i=1; i<N; ++i) {
-                if (biggest < cam[i]) {
-                    biggest = cam[i];
+            int[][] dp = new int[N+1][M+1];
+            for (int i=0; i<M+1; i++) {
+                dp[0][i] = cam[0];
+                System.out.println("dp[" + 0 + "][" + i + "]=" + dp[0][i]);
+            }
+            for (int n=1; n<N+1; ++n) {
+                dp[n][0] = getSum(cam, 0, n);
+                for (int m=1; m<M+1; ++m) {
+                    if (n < m) {
+                        dp[n][m] = dp[n][n];
+                        System.out.println("dp[" + n + "][" + m + "]=" + dp[n][m]);
+                        continue;
+                    }
+                    if (n == m) {
+                        dp[n][m] = getMax(cam, 0, n);
+                        System.out.println("dp[" + n + "][" + m + "]=" + dp[n][m]);
+                        continue;
+                    }
+                    int min = Integer.MAX_VALUE;
+                    for (int i=1; i<n; ++i) {
+                        int sum = getSum(cam, n-i+1, n);
+                        System.out.println("sum=" + sum);
+                        int tmp = Math.max(dp[n-i][m-1], sum);
+                        if (tmp < min) {
+                            min = tmp;
+                        }
+                    }
+                    dp[n][m] = min;
+                    System.out.println("dp[" + n + "][" + m + "]=" + dp[n][m]);
                 }
             }
-            while (true) {
-                if (isFit(N, M, cam, biggest)) {
-                    break;
-                }
-                biggest++;
-            }
-            System.out.println(biggest);
+
+            System.out.println(dp[N][M]);
+
+
         }
     }
+
+    public static int getMax(int[] array, int start, int end) {
+        int max = -1;
+        for (int i=start; i<end+1; ++i) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    public static int getSum(int[] array, int start, int end) {
+        int sum = 0;
+        for (int i=start; i<end+1; ++i) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
 
     public static boolean isFit(int N, int M, int[] cam, int testResult) {
         int index = 0;
